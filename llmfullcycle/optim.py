ff883b10afd3029615e90a7065fa5f973cba9a43
madankh <khadkasuman0000@gmail.com>
2026-08-02 14:27:45 +0545

    flow

--- llmfullcycle/optim.py
+++ llmfullcycle/optim.py
@@ -1,21 +1,22 @@
 import torch
 import torch.distributed as dist
 from torch import Tensor
+from llmfullcycle.common import COMPUTE_DTYPE
 
-@torch.compile(dynamic=False,  fullgraph=True)
+@torch.compile(dynamic=False, fullgraph=True)
 def adamw_step_fused(
-    p:Tensor,
-    grad:Tensor,
-    exp_avg:Tensor,
-    exp_avg_sqr:Tensor,
-    step_t:Tensor,
-    lr_t:Tensor,
-    beta1_t:Tensor,
-    beta2_t:Tensor,
-    eps_t:Tensor,
+    p:Tensor, 
+    grad:Tensor, 
+    exp_avg:Tensor, 
+    exp_avg_sqr:Tensor, 
+    step_t:Tensor, 
+    lr:Tensor, 
+    beta_1:Tensor, 
+    beta_2:Tensor, 
+    eps_t:Tensor, 
     wd_t:Tensor
-)-> None:
-   p.mul_(1 - lr_t * wd_t)
-   exp_avg.lerp_(grad, 1 - beta1_t)
-   exp_avg_sqr.lerp_(grad.square(), 1 - beta2_t)
-   
+    ):
+    p.mul_(1 - lr * wd_t)
+    exp_avg.lerp(grad, 1 - beta_1)
+    
+
--- llmfullcycle/script/base_train.py
+++ llmfullcycle/script/base_train.py
@@ -16,3 +16,38 @@ import torch
 import torch.distributed as dist
 
 from llmfullcycle.gpt import GPT, Linear,GPTConfig
+from llmfullcycle.helper import compute_init, compute_cleanup, print0, DummyWandb, print_banner, get_base_dir, autodetect_device_type, get_peak_flops, COMPUTE_DTYPE, COMPUTE_DTYPE_REASON, is_ddp_initialized
+print_banner()
+
+# CLI arguments
+parser = argparse.ArgumentParser(description="Pretrain base model")
+# logging
+parser.add_argument("--run", type=str, default="dummy", help="wandb run name")
+# runtime
+parser.add_argument("--device-type", type=str, default="", help="cuda|mps|cpu")
+# fp8 traning
+
+# model architecture
+
+# tranining horizon
+
+# optimization 
+
+# evaluation
+
+# Output
+
+
+# Compute init and wandb logging
+
+# wandb logging init
+
+# Flash attention status 
+
+# Build tokenizer
+
+# Initialize the model
+
+# Build the model , move to device , init the weights 
+
+
